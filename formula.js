397c83a3f28be4e43bcd5b3a7ac9909d3b47d682
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-02-23 17:04:14 +0530

    docs: formula.js

---
+++ formula.js
@@ -0,0 +1,153 @@
+for (let i = 0; i < rows; i++) {
+  for (let j = 0; j < cols; j++) {
+    let cell = document.querySelector(`.cell[rid="${i}"][cid="${j}"]`);
+    cell.addEventListener("blur", (e) => {
+      let address = addressBar.value;
+      let [activeCell, cellProp] = getCellAndCellProp(address);
+      let enteredData = activeCell.innerText;
+
+      if (enteredData === cellProp.value) return;
+
+      cellProp.value = enteredData;
+      // If data modifies remove P-C relation, formula empty, update children wit new hardcoded value
+      removeChildFromParent(cellProp.formula);
+      cellProp.formula = "";
+      updateChildrenCells(address);
+      //   console.log(cellProp);
+    });
+  }
+}
+
+let formulaBar = document.querySelector(".formula-bar");
+formulaBar.addEventListener("keydown", async (e) => {
+  let inputFormula = formulaBar.value;
+  if (e.key === "Enter" && inputFormula) {
+    // If change in formula, break old P-C relation, evaluate new formula, add new P-C relation
+    let address = addressBar.value;
+    let [cell, cellProp] = getCellAndCellProp(address);
+    if (inputFormula !== cellProp.formula)
+      removeChildFromParent(cellProp.formula);
+
+    addChildToGraphComponent(inputFormula, address);
+    // Check formula is cyclic or not, then only evaluate
+    // True -> cycle, Fasle -> Not cyclic
+    let cyclicResponse = isGraphCyclic(graphComponentMatrix);
+    if (cyclicResponse) {
+    //   alert("Your Formula is cyclic");
+
+    let response = confirm("Your formula is cyclic. Do you want to trace your path");
+    while(response === true) {
+        // Keep on tracking color until user is certisfied
+        await isGraphCyclicTracePath(graphComponentMatrix, cyclicResponse); // I want to calculate full iteration of color tracking, so i will attach wait here also
+        response = confirm("Your formula is cyclic. Do you want to trace your path");
+    }
+
+      removeChildFromGraphComponent(inputFormula, address);
+      return;
+    }
+
+    let evaluatedValue = evaluateFormula(inputFormula);
+
+    // To Update UI and cellProp in DB
+    setCellUIAndCellProp(evaluatedValue, inputFormula, address);
+    addChildToParent(inputFormula);
+    // console.log(sheetDB);
+
+    updateChildrenCells(address);
+  }
+});
+
+function addChildToGraphComponent(formula, childAddress) {
+  let [crid, ccid] = decodeRIDCIDrowAddress(childAddress);
+  let encodedFormula = formula.split(" ");
+  for (let i = 0; i < encodedFormula.length; i++) {
+    let asciiValue = encodedFormula[i].charCodeAt(0);
+    if (asciiValue >= 65 && asciiValue <= 95) {
+      let [prid, pcid] = decodeRIDCIDrowAddress(encodedFormula[i]);
+      // B1: A1 + 10
+      // rid -> i, cid -> j
+      graphComponentMatrix[prid][pcid].push([crid, ccid]);
+    }
+  }
+}
+
+function removeChildFromGraphComponent(formula, childAddress) {
+  let [crid, ccid] = decodeRIDCIDrowAddress(childAddress);
+  let encodedFormula = formula.split(" ");
+
+  for (let i = 0; i < encodedFormula.length; i++) {
+    let asciiValue = encodedFormula[i].charCodeAt(0);
+    if (asciiValue >= 65 && asciiValue <= 95) {
+      let [prid, pcid] = decodeRIDCIDrowAddress(encodedFormula[i]);
+      // B1: A1 + 10
+      // rid -> i, cid -> j
+      graphComponentMatrix[prid][pcid].pop();
+    }
+  }
+}
+
+function updateChildrenCells(parentAddress) {
+  let [parentCell, parentCellProp] = getCellAndCellProp(parentAddress);
+  let children = parentCellProp.children;
+
+  for (i = 0; i < children.length; i++) {
+    let childAddress = children[i];
+    let [childCell, childCellProp] = getCellAndCellProp(childAddress);
+    let childFormula = childCellProp.formula;
+
+    let evaluatedValue = evaluateFormula(childFormula);
+    setCellUIAndCellProp(evaluatedValue, childFormula, childAddress);
+    updateChildrenCells(childAddress);
+  }
+}
+
+function addChildToParent(formula) {
+  let childAddress = addressBar.value;
+
+  let encodedFormula = formula.split(" ");
+  for (let i = 0; i < encodedFormula.length; i++) {
+    let asciiValue = encodedFormula[i].charCodeAt(0);
+    if (asciiValue >= 65 && asciiValue <= 90) {
+      let [parentCell, parentCellProp] = getCellAndCellProp(encodedFormula[i]);
+      parentCellProp.children.push(childAddress);
+    }
+  }
+}
+
+function removeChildFromParent(formula) {
+  let childAddress = addressBar.value;
+
+  let encodedFormula = formula.split(" ");
+  for (let i = 0; i < encodedFormula.length; i++) {
+    let asciiValue = encodedFormula[i].charCodeAt(0);
+    if (asciiValue >= 65 && asciiValue <= 90) {
+      let [parentCell, parentCellProp] = getCellAndCellProp(encodedFormula[i]);
+      let idx = parentCellProp.children.indexOf(childAddress);
+      parentCellProp.children.splice(idx, 1);
+    }
+  }
+}
+
+function evaluateFormula(formula) {
+  let encodedFormula = formula.split(" ");
+  for (let i = 0; i < encodedFormula.length; i++) {
+    let asciiValue = encodedFormula[i].charCodeAt(0);
+    if (asciiValue >= 65 && asciiValue <= 90) {
+      let [cell, cellProp] = getCellAndCellProp(encodedFormula[i]);
+      encodedFormula[i] = cellProp.value;
+    }
+  }
+
+  let decodedFormula = encodedFormula.join(" ");
+  return eval(decodedFormula);
+}
+
+function setCellUIAndCellProp(evaluatedValue, formula, address) {
+  let [cell, cellProp] = getCellAndCellProp(address);
+
+  // UI Update
+  cell.innerText = evaluatedValue;
+  // DB Update
+  cellProp.value = evaluatedValue;
+  cellProp.formula = formula;
+}
